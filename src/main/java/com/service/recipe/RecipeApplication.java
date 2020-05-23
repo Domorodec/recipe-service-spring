8f0b96165acea0d568a109f1bbc9d13e5920985e
Martin Fabián <martin.fabian@centrum.cz>
2020-05-23 23:54:29 +0200

    adding kafka producer for sending message

--- src/main/java/com/service/recipe/RecipeApplication.java
+++ src/main/java/com/service/recipe/RecipeApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
@@ -23,5 +24,4 @@ public class RecipeApplication extends SpringBootServletInitializer {
     public EmailConstructor emailConstructor() {
         return new EmailConstructor();
     }
-
 }
