b06b1820dda2d3aafdd2a20041f2733e514d3a5a
Martin Fabián <martin.fabian@centrum.cz>
2020-08-16 07:45:34 +0200

    cross origin another try

---
+++ src/main/java/com/service/recipe/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.service.recipe.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String[] PUBLIC_MATCHERS = {"/login", "/users/register", "/recipes/list", "/h2-console/**"};
+
+    @Qualifier("userDetailsServiceImpl")
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        JwtAuthentication jwtAuthentication = new JwtAuthentication(authenticationManager());
+        jwtAuthentication.setFilterProcessesUrl(PUBLIC_MATCHERS[0]);
+        http.headers().frameOptions().disable();
+//        http.csrf().disable().cors().disable()
+        http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(jwtAuthentication)
+                .addFilterBefore(new JwtAuthorization(), UsernamePasswordAuthenticationFilter.class);
+    }
+}
